a316534e76db69998413143be8c09c771e727238
Gil Mandler <gil.mandler@gmail.com>
2025-10-26 20:06:39 -0400

    feat: add serializer, more tests

--- pyredis/protocol.py
+++ pyredis/protocol.py
@@ -1,47 +1,60 @@
 from typing import Tuple, TypeAlias, Optional, Any, ClassVar
-from abc import ABC, abstractmethod
 from dataclasses import dataclass, field, InitVar
 
 CRLF = b'\r\n'
-# TODO
-# Add serialization handler for each type (serialize)
+
+
+# TODO:
+# add empty and nil bulk string
+# b"$0\r\n\r\n" -> empty = serialized version is empty string
+# b"$-1\r\n" -> nil = serialized version is 'nil'
 # Add test cases for null bytes for array/bulk string and null data type
-# Listen conn, accept conn, rev and receive frame, send
-# Add commands PING and Echo
-class PyRedisType(ABC):
-    data = Any
-    prefix: ClassVar[bytes]
+# handle incomplete frames
+# Add command echo and ping
 
-    @abstractmethod
-    def decode(self, encoding='utf-8'):
-        raise NotImplemented
-    @abstractmethod
-    def serialize(self, encoding='utf-8'):
-        raise NotImplemented
-
-# b"+full\r\n"
-@dataclass(frozen=True)
-class SimpleString(PyRedisType):
-    prefix: ClassVar[bytes] = b'+'
+class PyRedisType:
+    prefix: ClassVar[str]
     data: bytes
+
     def decode(self, encoding='utf-8'):
-        return self.data.decode(encoding)
+        return self.data.decode()
+
+    def serialize(self):
+        return self.prefix.encode() + self._serialize_data() + CRLF
+
+    def _serialize_data(self):
+        return self.data
+
 
 # Error "-Error message\r\n”
 @dataclass(frozen=True)
 class Error(PyRedisType):
+    prefix = '-'
     data: bytes
-    def decode(self, encoding='utf-8'):
-        return self.data.decode(encoding)
+
+    def _serialize_data(self):
+        return self.data
+
+
+# b"+full\r\n"
+@dataclass(frozen=True)
+class SimpleString(PyRedisType):
+    prefix = '+'
+    data: bytes
 
 
 # Integer ":100\r\n"
 @dataclass(frozen=True)
 class Integer(PyRedisType):
+    prefix = ':'
     init_data: InitVar[bytes]
     data: int = field(init=False)
+
     def decode(self, encoding='utf-8'):
         return self.data
+
+    def _serialize_data(self):
+        return b'%(data)i' % {b'data': self.data}
 
     def __post_init__(self, init_data: bytes):
         try:
@@ -53,20 +66,32 @@ class Integer(PyRedisType):
 # Bulk String "$11\r\nhello world\r\n"
 @dataclass(frozen=True)
 class BulkString(PyRedisType):
+    prefix = '$'
     data: bytes
-    def decode(self, encoding='utf-8'):
-        return self.data.decode(encoding)
+
+    def _serialize_data(self):
+        return b'%(len)i%(CRLF)s%(data)s' % {b'len': len(self.data), b'CRLF': CRLF, b'data': self.data}
 
 
 # Arrays "*2\r\n:1\r\n:2\r\n"
 @dataclass(frozen=True)
 class Array(PyRedisType):
+    prefix = '*'
     data: list['Frame']
+
     def decode(self, encoding='utf-8'):
         return [val.decode(encoding) for val in self.data]
 
+    def _serialize_data(self):
+        res = b''
+        for part in self.data:
+            res += part.serialize()
+        return b'%(len)i%(CRLF)s%(data)s' % {b'len': len(self.data), b'CRLF': CRLF, b'data': res}
+
+
 Frame: TypeAlias = SimpleString | Error | Integer | BulkString | Array
 ParseResult = Tuple[Optional[Frame], int]
+
 
 def parse_bulk_string(buffer: bytes, offset: int) -> Tuple[BulkString | None, int]:
     if buffer.rfind(CRLF) <= offset:
@@ -78,7 +103,7 @@ def parse_bulk_string(buffer: bytes, offset: int) -> Tuple[BulkString | None, in
     return BulkString(content), buffer.find(content) + length + 2
 
 
-def parse_arrays(buffer: bytes, offset: int) -> Tuple[Array | None, int]:
+def parse_array(buffer: bytes, offset: int) -> Tuple[Array | None, int]:
     count = int(buffer[0:offset])
 
     if buffer[offset + 1:].count(CRLF) < count:
@@ -102,15 +127,17 @@ def parse_frame(buffer: bytes) -> ParseResult:
 
     size = delim + 2
     match chr(buffer[0]):
-        case '+':
+        case SimpleString.prefix:
             return SimpleString(buffer[1:delim]), size
-        case '-':
+        case Error.prefix:
             return Error(buffer[1:delim]), size
-        case ':':
+        case Integer.prefix:
             return Integer(buffer[1:delim]), size
-        case '$':
+        case BulkString.prefix:
             return parse_bulk_string(buffer[1:], delim)
-        case '*':
-            return parse_arrays(buffer[1:], delim)
+        case Array.prefix:
+            return parse_array(buffer[1:], delim)
+        case _:
+            return None, 0
 
-    return None, 0
+
--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -1,13 +1,15 @@
 import pytest
-from pyredis.protocol import parse_frame, SimpleString, Error, BulkString, Array, Integer
+from pyredis.protocol import parse_frame, SimpleString, Error, BulkString, Array, Integer, CRLF
+
 
 @pytest.mark.parametrize("buffer, expected", [
+    (b'none', (None, 0)),
     (b"+part", (None, 0)),
     (b"+full\r\n", (SimpleString(b"full"), 7)),
     (b"+full\r\n+part", (SimpleString(b"full"), 7)),
     (b":100", (None, 0)),
-    (b":100\r\n", (Integer(100), 6)),
-    (b":100\r\n:200", (Integer(100), 6)),
+    (b":100\r\n", (Integer(b'100'), 6)),
+    (b":100\r\n:200", (Integer(b'100'), 6)),
     (b"-parterror", (None, 0)),
     (b"-Error\r\n", (Error(b"Error"), 8)),
     (b"-Error\r\n+part", (Error(b"Error"), 8)),
@@ -15,44 +17,63 @@ from pyredis.protocol import parse_frame, SimpleString, Error, BulkString, Array
     (b"$5\r\nredis\r\n", (BulkString(b"redis"), 10)),
     (b"$5\r\nredis\r\n$4\r\npart", (BulkString(b"redis"), 10)),
     (b"*2\r\n:1\r\n:2", (None, 0)),
-    (b"*2\r\n:1\r\n:2\r\n", (Array([Integer(1), Integer(2)]), 12)),
-    (b"*2\r\n:1\r\n:2\r\n*2\r\n:3", (Array([Integer(1), Integer(2)]), 12)),
+    (b"*2\r\n:1\r\n:2\r\n", (Array([Integer(b'1'), Integer(b'2')]), 12)),
+    (b"*2\r\n:1\r\n:2\r\n*2\r\n:3", (Array([Integer(b'1'), Integer(b'2')]), 12)),
     (b"*3\r\n:1\r\n:2\r\n*1\r\n+full\r\n", (Array([Integer(b"1"), Integer(b"2"), Array([SimpleString(b"full")])]), 23)),
 ])
-
 def test_parse_frame(buffer, expected):
     got = parse_frame(buffer)
     assert got == expected
+
+
+def test_error_decode():
+    e = Error(b'Error message')
+    assert e.decode() == 'Error message'
 
 
 def test_simple_string_decode():
     s = SimpleString(b'OK')
     assert s.decode() == 'OK'
 
-def test_error_decode():
-    e = Error(b'Error message')
-    assert e.decode('utf-8') == 'Error message'
-    
-    # Test with a different encoding
-    e_jp = Error('エラー'.encode('shift-jis'))
-    assert e_jp.decode('shift-jis') == 'エラー'
 
 def test_integer_decode():
     # The decode method for Integer should just return the number itself.
     i = Integer(123)
     assert i.decode() == 123
 
+
 def test_bulk_string_decode():
     b = BulkString(b'hello world')
     assert b.decode() == 'hello world'
 
+
 def test_array_decode():
     arr = Array([
         BulkString(b'hello'),
-        Integer(42),
+        Integer(b'42'),
         SimpleString(b'OK'),
-        Array([Integer(1), Integer(2)])
+        Array([Integer(b'1'), Integer(b'2')])
     ])
-    
+
     expected = ['hello', 42, 'OK', [1, 2]]
     assert arr.decode() == expected
+
+def test_error_serialize():
+    e = Error(b'Error message')
+    assert e.serialize() == b'-Error message%(CRLF)s' % {b'CRLF': CRLF}
+
+def test_simple_string_serialize():
+    s = SimpleString(b'OK')
+    assert s.serialize() == b'+OK%(CRLF)s' % {b'CRLF': CRLF}
+
+def test_integer_serialize():
+    i = Integer(b'10')
+    assert i.serialize() == b':10%(CRLF)s' % {b'CRLF': CRLF}
+
+def test_bulk_string_serialize():
+    b = BulkString(b'full')
+    assert b.serialize() == b'$4%(CRLF)sfull%(CRLF)s' % {b'CRLF': CRLF}
+
+def test_array_serialize():
+    a = Array([Integer(b"1"), Integer(b"2"), Array([SimpleString(b"full")]),BulkString(b'full')])
+    assert a.serialize() == b'*4\r\n:1\r\n:2\r\n*1\r\n+full\r\n\r\n$4\r\nfull\r\n\r\n'
